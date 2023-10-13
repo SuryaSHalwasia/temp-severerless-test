e6a973daebf1cd3c30a4960d7451cdc516da6e79
SuryaSHalwasia <suryahalwasia5@gmail.com>
2023-10-13 12:06:34 -0400

    Small fix

--- functions/complete-product.js
+++ functions/complete-product.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 const airtable = new Airtable({apiKey:process.env.AIRTABLE_API_KEY})
 .base('appWHHC6IaPOMVxrV').table('products')
 
-//domain/.netlify/hello
 exports.handler = async(event,context) =>{
     const {id} = event.queryStringParameters
     if(id){
